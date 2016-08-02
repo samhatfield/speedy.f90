e36566d6a33529d51feb4cfd1404d4471b2bb76a
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2016-08-02 12:22:58 +0100

    Converted ini_stepone to F90

---
+++ source/ini_stepone.f90
@@ -0,0 +1,35 @@
+subroutine stepone
+    ! subroutine stepone
+    !
+    ! purpose : call initialization of semi-implicit scheme 
+    !           and perform initial time step
+
+    use tsteps, only: delt, delt2, alph, rob, wil, istart
+
+    implicit none
+
+    integer :: iitest = 1
+    real :: delth
+
+    if (iitest == 1) print *, ' instep: initial time step'
+
+    if (istart == 0) then
+
+      delth = 0.5 * delt
+
+      if (iitest == 1) print *, ' semi-impl. initialization'
+      call impint(delth, alph)
+
+      if (iitest == 1) print *, ' forward half-step'
+      call step(1, 1, delth, alph, rob, wil)
+
+      if (iitest == 1) print *, ' semi-impl. initialization'
+      call impint(delt, alph)
+
+      if (iitest == 1) print *, ' leapfrog half-step'
+      call step(1, 2, delt, alph, rob, wil)
+    end if
+
+    if (iitest == 1) print *, ' semi-impl. initialization'
+    call impint(delt2, alph)
+end
