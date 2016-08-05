f8391a4554c542f51bd4192be57151cc7b39e5e1
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2016-08-05 16:59:22 +0100

    Found type bug in com_date
    I was initialising two variables as INTEGER instead of REAL. This is why
    you use implicit none!!

--- source/com_date.h
+++ source/com_date.h
@@ -1,6 +1,7 @@
 !
 !   /DATE1/: date and time variables (updated in NEWDATE)
-      INTEGER IYEAR, IMONTH, IDAY, IMONT1, TMONTH, TYEAR
+      INTEGER IYEAR, IMONTH, IDAY, IMONT1
+      REAL TMONTH, TYEAR
       COMMON /DATE1/ IYEAR, IMONTH, IDAY, IMONT1, TMONTH, TYEAR
 
 !   /DATE2/: calendar set-up (initialized in NEWDATE)
