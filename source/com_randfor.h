56aab8e3be067c5330867d9b7cd15efca6876b30
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2016-08-08 14:58:33 +0100

    Converted ini_inirdf

--- source/com_randfor.h
+++ source/com_randfor.h
@@ -1,3 +1,3 @@
-C--
-C--   /RANDF/ Random diabatic forcing (initial. in INIRDF, modified by XS_RDF))
+!   /RANDF/ Random diabatic forcing (initial. in INIRDF, modified by XS_RDF))
+         REAL RANDFH, RANDFV
          COMMON /RANDF/ RANDFH(NLON,NLAT,2), RANDFV(NLAT,NLEV,2)
