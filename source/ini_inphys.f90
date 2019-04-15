28f58a9c7d1e21b3b798167ecf69991f39f0e58f
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2019-04-15 14:36:04 +0100

    Simplify ini_inphys.f90

--- source/ini_inphys.f90
+++ source/ini_inphys.f90
@@ -1,27 +1,20 @@
+! Initialize common blocks for physical parametrization routines
 subroutine inphys(hsg,rlat)
-    !
-    ! subroutine inphys (hsg,ppl,rlat)
-    !
-    ! Purpose: Initialize common blocks for physical parametrization routines
-    ! Input :  hsg  : sigma at half levels
-    !          rlat : gaussian-grid latitudes
-    ! Initialized common blocks: phycon, fsiglt, forcon,
-    !                            cnvcon, lsccon, radcon, sflcon, vdicon
+    ! hsg  : sigma at half levels
+    ! rlat : gaussian-grid latitudes
 
     use mod_atparam
     use mod_physcon
 
     implicit none
 
-    integer, parameter :: nlon = ix, nlat = il, nlev = kx, ngp = nlon*nlat
-
-    real :: hsg(0:nlev), rlat(nlat)  
+    real :: hsg(0:kx), rlat(il)
     integer :: j, k
 
     ! 1.2 Functions of sigma and latitude
     sigh(0) = hsg(0)
 
-    do k = 1, nlev
+    do k = 1, kx
         sig(k)  = 0.5*(hsg(k)+hsg(k-1))
         sigl(k) = log(sig(k))
         sigh(k) = hsg(k)
@@ -30,19 +23,19 @@ subroutine inphys(hsg,rlat)
         grdscp(k) = grdsig(k)/cp
     end do
 
-    ! Weights for vertical interpolation at half-levels(1,nlev) and surface
+    ! Weights for vertical interpolation at half-levels(1,kx) and surface
     ! Note that for phys.par. half-lev(k) is between full-lev k and k+1
     ! Fhalf(k) = Ffull(k)+WVI(K,2)*(Ffull(k+1)-Ffull(k))
-    ! Fsurf = Ffull(nlev)+WVI(nlev,2)*(Ffull(nlev)-Ffull(nlev-1))
-    do k = 1, nlev-1
+    ! Fsurf = Ffull(kx)+WVI(kx,2)*(Ffull(kx)-Ffull(kx-1))
+    do k = 1, kx-1
         wvi(k,1) = 1./(sigl(k+1)-sigl(k))
         wvi(k,2) = (log(sigh(k))-sigl(k))*wvi(k,1)
     end do
 
-    wvi(nlev,1) = 0.
-    wvi(nlev,2) = (log(0.99)-sigl(nlev))*wvi(nlev-1,1)
+    wvi(kx,1) = 0.
+    wvi(kx,2) = (log(0.99)-sigl(kx))*wvi(kx-1,1)
 
-    do j = 1, nlat
+    do j = 1, il
         slat(j) = sin(rlat(j))
         clat(j) = cos(rlat(j))
     end do
