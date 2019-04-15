853b195d94495fd05264fa8b41d3d399244d8d9f
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2019-04-15 14:15:56 +0100

    Simplified ini_agcm_init.f90

--- source/ini_agcm_init.f90
+++ source/ini_agcm_init.f90
@@ -1,26 +1,15 @@
-subroutine agcm_init(inidate, ntimes, irstart, ndays)
-    !   subroutine agcm_init (inidate,ntimes,irstart,ndays)
-    !
-    !   purpose: initialization of atmos. model and coupling interface 
-    !
-
+! Initialization of atmospheric model and coupling interface
+subroutine agcm_init(ndays)
     use mod_cpl_flags, only: icsea, isstan
     use mod_tsteps
     use mod_date, only: newdate, ndaytot, iyear, imonth, iday, ihour
 
     implicit none
 
-    ! input (reset by input/include files if inidate = 0):
-    integer, intent(in) :: inidate     ! initial date yyyymm
-    integer, intent(in) :: ntimes      ! integr. length in months (< 0) or days (> 0)
-    integer, intent(in) :: irstart     ! restart flag: 0 = no, > 0 = yes
+    ! Total number of integration days
+    integer, intent(inout) :: ndays
 
-    ! output:
-    integer, intent(inout) :: ndays       ! total no. of integration days
-
-    print *, ' hallo from speedy_agcm'
-
-    ! 1. set run initial time, duration, time-stepping and coupling options
+    ! Read mode from fort.2 file
     read (2,*) istart
 
     ! Read date from fort.2 file
@@ -33,24 +22,24 @@ subroutine agcm_init(inidate, ntimes, irstart, ndays)
 
     call newdate(0)
 
-    print *, 'start date ', iyear, imonth, iday, ihour
+    print *, 'Start date ', iyear, imonth, iday, ihour
 
     isst0 = (iyear0 - issty0) * 12 + imont0
 
     ndays = ndaytot
 
-    ! check consistency of coupling and prescribed SST anomaly flags
+    ! Check consistency of coupling and prescribed SST anomaly flags
     if (icsea >= 4) isstan = 1
 
-    ! 2. initialization of atmospheric model constants and variables
+    ! Initialization of atmospheric model constants and variables
     call ini_atm()
 
-    ! 3. initialization of coupled modules (land, sea, ice)
+    ! Initialization of coupled modules (land, sea, ice)
     call ini_coupler(istart)
 
-    ! 4. set up the forcing fields for the first time step
+    ! Set up the forcing fields for the first time step
     call fordate(0)
 
-    ! 5. do the initial (2nd-order) time step, initialize the semi-impl. scheme
+    ! Do the initial (2nd-order) time step, initialize the semi-implicit scheme
     call stepone
 end subroutine
