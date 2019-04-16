b4ea64cfbe7c6ff5ef97323ea925f9589f335c8d
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2019-04-16 16:06:27 +0100

    Simplify run script

--- run.sh
+++ run.sh
@@ -1,66 +1,39 @@
 #!/bin/bash
 
-# $1 = resolution (eg t21, t30)
-# $2 = experiment no. (eg 111)
-# $3 = experiment no. for restart file ( 0 = no restart )
-# $4 = make only, and don't run? ("make" for yes, "run" for no)
-
-
-if [ $# -ne 4 ] ; then
-    echo 'Usage: '$0' resol. exp_no. restart_no make_only_or_run' 1>&2
-    exit 1
-fi
-
 # Define directory names
-UT=`pwd`
-SRC=$UT/source
-TMP=$UT/tmp
-mkdir -p $UT/output/exp_$2
-OUT=$UT/output/exp_$2
-CD=$UT/output/exp_$3
+ROOT=`pwd`
+SRC=$ROOT/source
+RUNDIR=$ROOT/rundir
+CLIM=$ROOT/data/bc/t30/clim
+ANOM=$ROOT/data/bc/t30/anom
 
-# Copy files from basic version directory
+# Make run directory
+rm -rf $RUNDIR
+mkdir -p $RUNDIR
+cd $RUNDIR
 
-echo "copying from $SRC/source to $TMP"
-
-mkdir -p $TMP
-cd $TMP
-rm *
-
-cp $SRC/*.f90      $TMP/
-cp $SRC/*.h      $TMP/
-cp $SRC/*.s      $TMP/
-cp $SRC/makefile $TMP/
-
-# Set experiment no. and restart file (if needed)
-
-echo $3 >  fort.2
-echo $2 >> fort.2
-
-if [ $3 != 0 ] ; then
-  echo "link restart file atgcm$3.rst to fort.3"
-  ln -s $CD/atgcm$3.rst fort.3
-fi
+# Copy source files
+cp $SRC/*.f90    $RUNDIR
+cp $SRC/*.h      $RUNDIR
+cp $SRC/makefile $RUNDIR
 
 # Link input files
+ln -s $CLIM/sfc.grd   fort.20
+ln -s $CLIM/sst.grd   fort.21
+ln -s $CLIM/icec.grd  fort.22
+ln -s $CLIM/stl.grd   fort.23
+ln -s $CLIM/snowd.grd fort.24
+ln -s $CLIM/swet.grd  fort.26
+ln -s $ANOM/ssta.grd  fort.30
 
-echo 'link input files to fortran units'
-
-./inpfiles.s $1
-
-ls -l fort.*
-
-echo ' compiling at_gcm - calling make'
-
-make clean
-make imp.exe || { echo "Compilation failed"; exit 1; }
-
-if [ $4 == make ] ; then
-    exit 0
-fi
+# Compile SPEEDY and delete source files
+echo 'Compiling SPEEDY'
+make -s clean
+make -s imp.exe || { echo "Compilation failed"; exit 1; }
+rm *.f90 *.h *.o makefile *.mod
 
 # Write date input file
-cat << EOF > fort.2
+cat << EOF >> fort.2
 1982
 01
 01
@@ -73,18 +46,5 @@ cat << EOF > fort.2
 00
 EOF
 
-time ./imp.exe | tee out.lis
-
-mv out.lis $OUT/atgcm$2.lis
-mv fort.10 $OUT/atgcm$2.rst
-
-mv at*$2.ctl   $OUT
-mv at*$2_*.grd $OUT
-
-mv day*$2.ctl   $OUT
-mv day*$2_*.grd $OUT
-
-mv *.grd $OUT
-cp ../ctl_files/yyyymmddhh.ctl $OUT
-
-cd $OUT
+# Run SPEEDY
+time ./imp.exe | tee output.txt
