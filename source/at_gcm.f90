77f99514088af662e533da590705a363800c2ceb
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2019-04-16 09:42:19 +0100

    Simplify at_gcm.f90

--- source/at_gcm.f90
+++ source/at_gcm.f90
@@ -1,47 +1,34 @@
 program agcm_main
-    use mod_date, only: ndaytot
+    use mod_tsteps, only: nsteps
+    use mod_date, only: ndaytot, iyear, imonth, iday
 
     implicit none
 
-    integer :: jday
+    integer :: jday, istep
 
     ! Initialization
     call agcm_init()
 
-    print *, 'integration length in days: ', ndaytot
+    write (*,'(A28, I5)') 'Integration length in days: ', ndaytot
 
     ! Do loop over total number of integration days
     do jday = 1, ndaytot
-        ! Run atmospheric model for 1 day
-        call agcm_1day(jday)
+        if (iday == 1) write(*,'(A14, I4, I0.2, I0.2)') 'Current date: ', iyear, imonth, iday
+
+		! Increment time step counter
+        istep = 1 + (jday - 1) * nsteps
+
+		! Set forcing terms according to date
+        call fordate(1)
+
+		! Set daily-average flux arrays to zero
+        call dmflux(0)
+
+		! Integrate the atmospheric model for 1 day
+        call stloop(istep)
 
         ! Exchange data with coupler
         call agcm_to_coupler(jday)
         call coupler_to_agcm(jday)
     enddo
-end
-
-! Perform atmospheric model integration for 1 day
-subroutine agcm_1day(jday)
-
-    use mod_tsteps, only: nsteps
-    use mod_date, only: iyear, imonth, iday
-
-    implicit none
-
-    integer, intent(in) :: jday
-    integer :: istep
-
-    if (iday == 1) print *, ' start of year/month = ', iyear, imonth
-
-    istep = 1 + (jday - 1) * nsteps
-
-    ! 1. set forcing terms according to date
-    call fordate(1)
-
-    ! 2. set daily-average flux arrays to zero
-    call dmflux(0)
-
-    ! 3. integrate the atmospheric model for 1 day
-    call stloop(istep)
 end
