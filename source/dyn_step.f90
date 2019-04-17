fd9fdfce0f176f0f3d39a143c0ab54187681eca0
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2019-04-17 09:23:57 +0100

    Neaten dyn_grtend.f90 a bit

--- source/dyn_step.f90
+++ source/dyn_step.f90
@@ -1,23 +1,16 @@
-subroutine step(j1,j2,dt,alph)
-    !   subroutine step (j1,j2,dt,alph)
-    !
-    !   Purpose: perform one time step starting from F(1) and F(2)
-    !            and using the following scheme:
-    !
-    !   Fnew = F(1) + DT * [ T_dyn(F(J2)) + T_phy(F(1)) ]
-    !   F(1) = (1-2*eps)*F(J1) + eps*[F(1)+Fnew]
-    !   F(2) = Fnew
-    !
-    !   Input:
-    !   If J1=1, J2=1 : forward time step (eps=0)
-    !   If J1=1, J2=2 : initial leapfrog time step (eps=0)
-    !   If J1=2, J2=2 : leapfrog time step with time filter (eps=ROB)
-    !   DT = time step (if DT < or = 0, tendencies are computed but
-    !                   no time stepping is performed)
-    !   alph = 0   : forward step for gravity wave terms
-    !   alph = 1   : backward implicit step for g.w.
-    !   alph = 0.5 : centered implicit step for g.w.
-
+! Perform one time step starting from F(1) and F(2) and using the following scheme:
+! Fnew = F(1) + DT * [ T_dyn(F(J2)) + T_phy(F(1)) ]
+! F(1) = (1-2*eps)*F(J1) + eps*[F(1)+Fnew]
+! F(2) = Fnew
+! Input:
+! If j1 == 1, j2 == 1 : forward time step (eps = 0)
+! If j1 == 1, j2 == 2 : initial leapfrog time step (eps = 0)
+! If j1 == 2, j2 == 2 : leapfrog time step with time filter (eps = ROB)
+! dt = time step
+! alph = 0   : forward step for gravity wave terms
+! alph = 1   : backward implicit step for gravity wave terms
+! alph = 0.5 : centered implicit step for gravity wave terms
+subroutine step(j1, j2, dt, alph)    !
     use mod_dyncon0, only: tdrs
     use mod_atparam
     use mod_dynvar
@@ -28,7 +21,7 @@ subroutine step(j1,j2,dt,alph)
 
     integer, intent(in) :: j1, j2
     real, intent(in) :: dt, alph
-    complex, dimension(mx,nx,kx) ::  ordt, divdt, tdt, vordt
+    complex, dimension(mx,nx,kx) ::  vordt, divdt, tdt
     complex :: psdt(mx,nx), trdt(mx,nx,kx,ntr)
     real :: eps, sdrag
 
@@ -36,91 +29,96 @@ subroutine step(j1,j2,dt,alph)
 
     integer :: n, itr, k, m
 
-    ! 1. Computation of grid-point tendencies
-    ! (converted to spectral at the end of GRTEND)
-    call grtend(vordt,divdt,tdt,psdt,trdt,1,j2)
+    ! =========================================================================
+    ! Computation of grid-point tendencies (converted to spectral at the end of
+    ! grtend)
+    ! =========================================================================
 
-    ! 2. Computation of spectral tendencies
-    if (alph.eq.0.) then
-        call sptend(divdt,tdt,psdt,j2)
+    call grtend(vordt, divdt, tdt, psdt, trdt, 1, j2)
+
+    ! =========================================================================
+    ! Computation of spectral tendencies
+    ! =========================================================================
+    if (alph < 0.5) then
+        call sptend(divdt, tdt, psdt, j2)
     else
-        call sptend(divdt,tdt,psdt,1)
+        call sptend(divdt, tdt, psdt, 1)
 
-        ! implicit correction
-        call implic(divdt,tdt,psdt)
+        ! Implicit correction
+        call implic(divdt, tdt, psdt)
     endif
 
-    ! 3. Horizontal diffusion
+    ! =========================================================================
+    ! Horizontal diffusion
+    ! =========================================================================
 
-    ! 3.1 Diffusion of wind and temperature
-    call hordif(kx,vor,vordt,dmp, dmp1)
-    call hordif(kx,div,divdt,dmpd,dmp1d)
+    ! Diffusion of wind and temperature
+    call hordif(kx, vor, vordt, dmp,  dmp1)
+    call hordif(kx, div, divdt, dmpd, dmp1d)
 
-    do k=1,kx
-        do m=1,mx
-            do n=1,nx
-                ctmp(m,n,k) = t(m,n,k,1)+tcorh(m,n)*tcorv(k)
-            enddo
-        enddo
-    enddo
+    do k = 1, kx
+        do m = 1, mx
+            do n = 1, nx
+                ctmp(m,n,k) = t(m,n,k,1) + tcorh(m,n)*tcorv(k)
+            end do
+        end do
+    end do
 
     call hordif(kx,ctmp,tdt,dmp,dmp1)
 
-    ! 3.2 Stratospheric diffusion and zonal wind damping
-    sdrag = 1./(tdrs*3600.)
-    do n = 1,nx
-        vordt(1,n,1) = vordt(1,n,1)-sdrag*vor(1,n,1,1)
-        divdt(1,n,1) = divdt(1,n,1)-sdrag*div(1,n,1,1)
-    enddo
+    ! Stratospheric diffusion and zonal wind damping
+    sdrag = 1.0/(tdrs*3600.0)
+    do n = 1, nx
+        vordt(1,n,1) = vordt(1,n,1) - sdrag*vor(1,n,1,1)
+        divdt(1,n,1) = divdt(1,n,1) - sdrag*div(1,n,1,1)
+    end do
 
-    call hordif(1,vor, vordt,dmps,dmp1s)
-    call hordif(1,div, divdt,dmps,dmp1s)
-    call hordif(1,ctmp,tdt,  dmps,dmp1s)
+    call hordif(1, vor,  vordt, dmps, dmp1s)
+    call hordif(1, div,  divdt, dmps, dmp1s)
+    call hordif(1, ctmp, tdt,   dmps, dmp1s)
 
-    ! 3.4 Diffusion of tracers
-    do k=1,kx
-        do m=1,mx
-            do n=1,nx
-                ctmp(m,n,k) = tr(m,n,k,1,1)+qcorh(m,n)*qcorv(k)
-            enddo
-        enddo
-    enddo
+    ! Diffusion of tracers
+    do k = 1, kx
+        do m = 1, mx
+            do n = 1, nx
+                ctmp(m,n,k) = tr(m,n,k,1,1) + qcorh(m,n)*qcorv(k)
+            end do
+        end do
+    end do
 
-    call hordif(kx,ctmp,trdt,dmpd,dmp1d)
+    call hordif(kx, ctmp, trdt, dmpd, dmp1d)
 
-    if (ntr.gt.1) then
-        do itr=2,ntr
-            call hordif(kx,tr(1,1,1,1,itr),trdt(1,1,1,itr),dmp,dmp1)
+    if (ntr > 1) then
+        do itr = 2, ntr
+            call hordif(kx, tr(:,:,:,1,itr), trdt(:,:,:,itr), dmp, dmp1)
         enddo
     endif
 
-    ! 4. Time integration with Robert filter
-    if (dt.le.0.) return
+    ! =========================================================================
+    ! Time integration with Robert filter
+    ! =========================================================================
 
-    if (j1.eq.1) then
-        eps = 0.
+    if (j1 == 1) then
+        eps = 0.0
     else
         eps = rob
     endif
 
-    call timint(j1,dt,eps,wil,1,ps,psdt)
+    call timint(j1, dt, eps, wil, 1, ps, psdt)
 
-    call timint(j1,dt,eps,wil,kx,vor,vordt)
-    call timint(j1,dt,eps,wil,kx,div,divdt)
-    call timint(j1,dt,eps,wil,kx,t,  tdt)
+    call timint(j1, dt, eps, wil, kx, vor, vordt)
+    call timint(j1, dt, eps, wil, kx, div, divdt)
+    call timint(j1, dt, eps, wil, kx, t,  tdt)
 
-    do itr=1,ntr
-        call timint(j1,dt,eps,wil,kx,tr(1,1,1,1,itr),trdt(1,1,1,itr))
+    do itr = 1, ntr
+        call timint(j1, dt, eps, wil, kx, tr(:,:,:,1,itr), trdt(:,:,:,itr))
     enddo
 end
 
+! Add horizontal diffusion tendency of FIELD to spectral tendency FDT at NLEV
+! levels using damping coefficients DMP and DMP1
 subroutine hordif(nlev,field,fdt,dmp,dmp1)
-    !   Aux. subr. HORDIF (NLEV,FIELD,FDT,DMP,DMP1)
-    !   Purpose : Add horizontal diffusion tendency of FIELD
-    !             to spectral tendency FDT at NLEV levels
-    !             using damping coefficients DMP and DMP1
-
-    USE mod_atparam
+    use mod_atparam
 
     implicit none
 
@@ -130,18 +128,15 @@ subroutine hordif(nlev,field,fdt,dmp,dmp1)
     real, intent(in) :: dmp(mxnx), dmp1(mxnx)
     integer :: k, m
 
-    do k=1,nlev
-        do m=1,mxnx
-            fdt(m,k)=(fdt(m,k)-dmp(m)*field(m,k))*dmp1(m)
-        enddo
-    enddo
+    do k = 1, nlev
+        do m = 1, mxnx
+            fdt(m,k) = (fdt(m,k) - dmp(m)*field(m,k))*dmp1(m)
+        end do
+    end do
 end
 
-subroutine timint(j1,dt,eps,wil,nlev,field,fdt)
-    !  Aux. subr. timint (j1,dt,eps,wil,nlev,field,fdt)
-    !  Purpose : Perform time integration of field at nlev levels
-    !            using tendency fdt
-
+! Perform time integration of field at nlev levels using tendency fdt
+subroutine timint(j1, dt, eps, wil, nlev, field, fdt)
     use mod_atparam
 
     implicit none
@@ -154,24 +149,24 @@ subroutine timint(j1,dt,eps,wil,nlev,field,fdt)
     complex :: fnew(mxnx)
     integer :: k, m
 
-    eps2 = 1.-2.*eps
+    eps2 = 1.0 - 2.0*eps
 
-    if (ix.eq.iy*4) then
-        do k=1,nlev
+    if (ix == iy*4) then
+        do k = 1, nlev
             call trunct(fdt(1,k))
-        enddo
-    endif
+        end do
+    end if
 
-    ! The actual leap frog with the robert filter
-    do k=1,nlev
-        do m=1,mxnx
+    ! The actual leap frog with the Robert filter
+    do k = 1, nlev
+        do m =1, mxnx
             fnew (m)     = field(m,k,1) + dt*fdt(m,k)
             field(m,k,1) = field(m,k,j1) +  wil*eps*(field(m,k,1)&
-                & -2*field(m,k,j1)+fnew(m))
+                & - 2*field(m,k,j1)+fnew(m))
 
             ! and here comes Williams' innovation to the filter
-            field(m,k,2) = fnew(m)-(1-wil)*eps*(field(m,k,1)&
-                &-2*field(m,k,j1)+fnew(m))
-        enddo
-    enddo
+            field(m,k,2) = fnew(m) - (1-wil)*eps*(field(m,k,1)&
+                & - 2*field(m,k,j1) + fnew(m))
+        end do
+    end do
 end
