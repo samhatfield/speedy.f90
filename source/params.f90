ac210a5efd57d19475aafbaabd19d09cacddc219
Sam Hatfield <samuel.hatfield@physics.ox.ac.uk>
2019-07-04 15:18:14 +0100

    Add documentation for parameters and constants files

--- source/params.f90
+++ source/params.f90
@@ -1,3 +1,6 @@
+!> author: Sam Hatfield, Fred Kucharski, Franco Molteni
+!  date: 04/07/2019
+!  For storing and initializing model parameters.
 module params
     implicit none
 
@@ -11,35 +14,37 @@ module params
     ! =========================================================================
 
     ! Model geometry parameters
-    integer, parameter :: trunc = 30         ! Spectral truncation
-    integer, parameter :: ix = 96            ! Number of longitudes
-    integer, parameter :: iy = 24, il = 2*iy ! Number of latitudes in hemisphere and full sphere
-    integer, parameter :: kx = 8             ! Number of vertical levels
-    integer, parameter :: nx = trunc+2       ! Number of total wavenumbers
-    integer, parameter :: mx = trunc+1       ! Number of zonal wavenumbers
-    integer, parameter :: ntr = 1            ! Number of tracers
+    integer, parameter :: trunc = 30   !! Spectral truncation total wavenumber
+    integer, parameter :: ix = 96      !! Number of longitudes
+    integer, parameter :: iy = 24      !! Number of latitudes in hemisphere
+    integer, parameter :: il = 2*iy    !! Number of latitudes in full sphere
+    integer, parameter :: kx = 8       !! Number of vertical levels
+    integer, parameter :: nx = trunc+2 !! Number of total wavenumbers for spectral storage arrays
+    integer, parameter :: mx = trunc+1 !! Number of zonal wavenumbers for spectral storage arrays
+    integer, parameter :: ntr = 1      !! Number of tracers (specific humidity is considered a
+                                       !! tracer)
 
     ! Time stepping parameters
-    integer, parameter :: nsteps = 36             ! Number of time steps in one day
-    real, parameter    :: delt = 86400.0 / nsteps ! Time step in seconds
-    real, parameter    :: rob = 0.05              ! Damping factor in Robert time filter
-    real, parameter    :: wil = 0.53              ! Parameter of Williams filter
-    real, parameter    :: alph = 0.5              ! Coefficient for semi-implicit computations
-                                                  ! 0 -> forward step for gravity wave terms,
-                                                  ! 1 -> backward implicit, 0.5 -> centered implicit
+    integer, parameter :: nsteps = 36           !! Number of time steps in one day
+    real, parameter    :: delt = 86400.0/nsteps !! Time step in seconds
+    real, parameter    :: rob = 0.05            !! Damping factor in Robert time filter
+    real, parameter    :: wil = 0.53            !! Parameter of Williams filter
+    real, parameter    :: alph = 0.5            !! Coefficient for semi-implicit computations
+                                                !! 0 -> forward step for gravity wave terms,
+                                                !! 1 -> backward implicit, 0.5 -> centered implicit
 
     ! Physics parameters
-    integer, parameter :: iseasc = 1           ! Seasonal cycle flag (0=no, 1=yes)
-    integer, parameter :: nstrad = 3           ! Period (number of steps) for shortwave radiation
-    logical, parameter :: sppt_on = .false.    ! Turn on SPPT?
-    integer, parameter :: issty0 = 1979        ! Starting year for SST anomaly file
+    integer, parameter :: iseasc = 1        !! Seasonal cycle flag (0=no, 1=yes)
+    integer, parameter :: nstrad = 3        !! Period (number of steps) for shortwave radiation
+    logical, parameter :: sppt_on = .false. !! Turn on SPPT?
+    integer, parameter :: issty0 = 1979     !! Starting year for SST anomaly file
 
     ! =========================================================================
     ! User-specified parameters (through the namelist file)
     ! =========================================================================
 
-    integer :: nstdia     ! Period (number of steps) for diagnostic print-out
-    integer :: nsteps_out ! Number of time steps between outputs
+    integer :: nstdia     !! Period (number of steps) for diagnostic print-out
+    integer :: nsteps_out !! Number of time steps between outputs
 
 contains
     !> Initializes user-defined parameters from namelist file.
--- source/physical_constants.f90
+++ source/physical_constants.f90
@@ -1,3 +1,6 @@
+!> author: Sam Hatfield, Fred Kucharski, Franco Molteni
+!  date: 04/07/2019
+!  For storing and initializing physical constants.
 module physical_constants
     use params
 
@@ -9,25 +12,26 @@ module physical_constants
     public sigl, sigh, grdsig, grdscp, wvi
 
     ! Physical constants for dynamics
-    real, parameter :: rearth = 6.371e+6
-    real, parameter :: omega  = 7.292e-05
-    real, parameter :: grav   = 9.81
-    real, parameter :: akap   = 2.0/7.0
-    real, parameter :: rgas   = akap*1004.0
+    real, parameter :: rearth = 6.371e+6    !! Radius of Earth (m)
+    real, parameter :: omega  = 7.292e-05   !! Rotation rate of Earth (rad/s)
+    real, parameter :: grav   = 9.81        !! Gravitational acceleration (m/s/s)
+    real, parameter :: akap   = 2.0/7.0     !! 1 - 1/gamma where gamma is the heat capacity ratio of
+                                            !! a perfect diatomic gas (7/5)
+    real, parameter :: rgas   = akap*1004.0 !! Gas constant per unit mass for dry air (J/K/kg)
 
     ! Physical constants for thermodynamics
-    real, parameter :: p0   = 1.e+5   ! Reference pressure
-    real, parameter :: rd   = 287.0   ! Gas constant for dry air
-    real, parameter :: cp   = 1004.0  ! Specific heat at constant pressure
-    real, parameter :: alhc = 2501.0  ! Latent heat of condensation, in J/g for consistency with
-                                      ! specific humidity in g/Kg
-    real, parameter :: alhs = 2801.0  ! Latent heat of sublimation
-    real, parameter :: sbc  = 5.67e-8 ! Stefan-Boltzmann constant
+    real, parameter :: p0   = 1.e+5   !! Reference pressure (Pa)
+    real, parameter :: rd   = 287.0   !! Gas constant per unit mass for dry air (J/K/kg)
+    real, parameter :: cp   = 1004.0  !! Specific heat at constant pressure (J/K/kg)
+    real, parameter :: alhc = 2501.0  !! Latent heat of condensation, in J/g for consistency with
+                                      !! specific humidity in g/Kg
+    real, parameter :: alhs = 2801.0  !! Latent heat of sublimation
+    real, parameter :: sbc  = 5.67e-8 !! Stefan-Boltzmann constant
 
     !   Functions of sigma and latitude (initial. in INPHYS)
-    real, dimension(kx)   :: sigl   ! Logarithm of full-level sigma
-    real, dimension(0:kx) :: sigh   ! Half-level sigma
-    real, dimension(kx)   :: grdsig ! g/(d_sigma p0) : to convert fluxes of u,v,q into d(u,v,q)/dt
-    real, dimension(kx)   :: grdscp ! g/(d_sigma p0 c_p): to convert energy fluxes into dT/dt
-    real, dimension(kx,2) :: wvi    ! Weights for vertical interpolation
+    real, dimension(kx)   :: sigl   !! Logarithm of full-level sigma
+    real, dimension(0:kx) :: sigh   !! Half-level sigma
+    real, dimension(kx)   :: grdsig !! g/(d_sigma p0) : to convert fluxes of u,v,q into d(u,v,q)/dt
+    real, dimension(kx)   :: grdscp !! g/(d_sigma p0 c_p): to convert energy fluxes into dT/dt
+    real, dimension(kx,2) :: wvi    !! Weights for vertical interpolation
 end module
